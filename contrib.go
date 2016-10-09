5c4f383ef59560bc57b2646a4ba0d9fb874cf021
insion <insion@live.com>
2016-10-09 12:54:52 +0800

    just for vodka v2+

--- contrib.go
+++ contrib.go
@@ -9,7 +9,7 @@ const VodkaCacheStoreKey = "VodkaCacheStore"
 func Store(value interface{}) Cache {
 	var cacher Cache
 	switch v := value.(type) {
-	case *vodka.Context:
+	case vodka.Context:
 		cacher = v.Get(VodkaCacheStoreKey).(Cache)
 		if cacher == nil {
 			panic("VodkaStore not found, forget to Use Middleware ?")
@@ -28,7 +28,7 @@ func Store(value interface{}) Cache {
 
 func VodkaCacher(opt Options) vodka.MiddlewareFunc {
 	return func(h vodka.HandlerFunc) vodka.HandlerFunc {
-		return func(c *vodka.Context) error {
+		return func(c vodka.Context) error {
 			tagcache, err := New(opt)
 			if err != nil {
 				return err
